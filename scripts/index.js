0bc3af1eca6e8d16e4bec15ec80b468155b26e0c
Mfreixanet <marcfreixanet@gmail.com>
2023-09-08 18:02:04 +0200

    sprint 6 corrections

--- scripts/index.js
+++ scripts/index.js
@@ -68,12 +68,12 @@ function closeByEscape(evt) {
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
-  document.removeEventListener("keydown", closeByEsacpe);
+  document.removeEventListener("keydown", closeByEscape);
 }
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
-  document.addEventListener("keydown", closeByEsacpe);
+  document.addEventListener("keydown", closeByEscape);
 }
 
 function renderCard(cardData, wrapper) {
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,63 +1,70 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-const showInputError = (formEl, inputEl, { inputErrorClass, errorClass }) => {
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.add(inputErrorClass);
   errorMessageEl.textContent = inputEl.validationMessage;
   errorMessageEl.classList.add(errorClass);
-};
+}
 
-const hideInputError = (formEl, inputEl, { inputErrorClass, errorClass }) => {
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.remove(inputErrorClass);
   errorMessageEl.textContent = "";
   errorMessageEl.classList.remove(errorClass);
-};
+}
 
-const hasInvalidInput = (inputList) => {
-  return !inputList.every((inputEl) => inputEl.validity.valid);
-};
-
-const toggleButtonState = (inputEls, submitButton, { inactiveButtonClass }) => {
-  let foundInvalid = false;
-  inputEls.forEach((inputEl) => {
-    if (!inputEl.validity.valid) {
-      foundInvalid = true;
-    }
-  });
-
-  if (hasInvalidInput(inputEls)) {
-    submitButton.classList.add(inactiveButtonClass);
-    submitButton.disabled = true;
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, options);
   } else {
-    submitButton.classList.remove(inactiveButtonClass);
-    submitButton.disabled = false;
+    hideInputError(formEl, inputEl, options);
   }
-};
+}
 
-const setEventListeners = (formEl, options) => {
-  const { inputSelector } = options;
-  const inputEls = Array.from(formEl.querySelectorAll(options.inputSelector));
-  const submitButton = formEl.querySelector(".popup__button");
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
 
+function disableButton(inputEls, submitButton, { inactiveButtonClass }) {
+  submitButton.classList.add(inactiveButtonClass);
+  submitButton.disabled = true;
+}
+
+function enableButton(inputEls, submitButton, { inactiveButtonClass }) {
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    return disableButton(inputEls, submitButton, { inactiveButtonClass });
+  }
+  return enableButton(inputEls, submitButton, { inactiveButtonClass });
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector, submitButtonSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(submitButtonSelector);
   inputEls.forEach((inputEl) => {
-    inputEl.addEventListener("input", (evt) => {
+    inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputEls, submitButton);
+      toggleButtonState(inputEls, submitButton, options);
     });
   });
-};
+}
 
-const enableValidation = (options) => {
-  const formEls = Array.from(document.querySelectorAll(options.formSelector));
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
-    formEl.addEventListener("submit", (evt) => {
-      evt.preventDefault();
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
     });
     setEventListeners(formEl, options);
   });
-};
+}
 
 const config = {
   formSelector: ".popup__form",
